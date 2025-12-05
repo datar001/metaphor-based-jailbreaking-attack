499b4af3053b4470751a1d8a6c25b04ece267a41
datar001 <62345916+datar001@users.noreply.github.com>
2025-12-05 22:07:15 +0800

    Update llama3_meta.py

--- llama/llama3_meta.py
+++ llama/llama3_meta.py
@@ -7,9 +7,9 @@ class MetaLlama(nn.Module):
     def __init__(self, model, cuda_id=0):
         super().__init__()
         if model == "llama3":
-            model_id = "/home/zcy/llama_model_hugging/Meta-Llama-3-8B-Instruct/"
+            model_id = "/home/xxx/llama_model_hugging/Meta-Llama-3-8B-Instruct/"
         elif model == "llama3-uncensored":
-            model_id = "/home/zcy/llama_model_hugging/lexi-llama-3-8B/"
+            model_id = "/home/xxx/llama_model_hugging/lexi-llama-3-8B/"
         else:
             raise ValueError("Model must be either llama3 or llama3-uncensored")
 
@@ -81,3 +81,4 @@ if __name__ == "__main__":
     ]
     results = meta_llama.generate_text(messages)
     print(results)
+
